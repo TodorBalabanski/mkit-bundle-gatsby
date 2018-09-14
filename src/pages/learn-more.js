7f97a215d6e0de46cf4d3304cdee9a0e5c3cdfe8
Stoyan Merdzhanov <stoyan.merdzhanov@gmail.com>
2018-09-15 01:00:33 +0300

    Prepare the two demo pages of the project

--- src/pages/learn-more.js
+++ src/pages/learn-more.js
@@ -5,15 +5,15 @@ import Layout from '../components/layout/layout';
 import Seo from '../components/seo/seo';
 
 const seo = {
-  pageTitle: 'Welcome'
+  pageTitle: 'Learn More'
 };
 
-const IndexPage = ({ location }) => (
+const MorePage = ({ location }) => (
   <Layout>
     <Seo {...seo} pathname={location.pathname} />
-    <h1>Index Page</h1>
-    <Link to="/learn-more">Learn More</Link>
+    <h1>Learn More Page</h1>
+    <Link to="/">Index</Link>
   </Layout>
 );
 
-export default IndexPage;
+export default MorePage;
